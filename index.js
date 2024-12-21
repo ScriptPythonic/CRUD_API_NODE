a690689ee2dc2ed4fd7d557f90fcd9c326d3a060
scriptpythonic <Quadribasit04@gmail.com>
2024-12-21 02:15:03 -0800

    added a new api to create and delete in dummy

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+const Product = require('./models/product.model')
 const mongoose = require('mongoose')
 
 
@@ -9,10 +11,53 @@ app.listen(3000,()=>{
 })
 
 app.get('/',(req,res)=>{
-    res.send('hello world from Basit ayomide')
+   res.send("welcome to the api created by scriptpythonic a python developer")
 })
 
-mongoose.connect('')
+
+// api end point for product => create
+app.post('/api/products',async(req,res)=>{
+    try{
+        const product = await Product.create(req.body)
+        res.status(200).json({message:"mesaage product created successfully",product})
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+// get all the product in the database
+app.get('/api/products', async(req,res)=>{
+    try{
+        const products = await Product.find()
+
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get("/api/products/:id",async(req,res)=>{
+    try{
+
+        const product = req.params.id
+        const fetch_product  = await Product.findById(product)
+        if(!fetch_product){
+            res.status(404).json({message:"unable to find the product"})
+        }
+
+        res.status(200).json({message:fetch_product})
+
+    }catch(error){
+        res.send(500).jsons({message: error.message})
+    }
+})
+
+app.delete("/api/products/:id",async(req,res)=>{
+
+})
+
+mongoose.connect('mongodb+srv://quadribasit04:Quadri123%40%40%40@cluster0.s208b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>{
     console.log('connected to database')
 })
